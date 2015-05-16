99fbd2d19b70ac311c942b9a7d83d3bdb66242f9
Jayesh Kitukale <jayeshkitukale@Jayeshs-Mac-mini.local>
2015-05-16 11:13:53 +0530

    Initial Commit

---
+++ CorpRooms/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CorpRooms
+//
+//  Created by Jayesh Kitukale on 5/16/15.
+//  Copyright (c) 2015 Tungsten. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CorpRooms/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CorpRooms
+//
+//  Created by Jayesh Kitukale on 5/16/15.
+//  Copyright (c) 2015 Tungsten. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
